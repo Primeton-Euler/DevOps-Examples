81d69a152eb62acdc0ed98696ece58b6eeb53d20
ZhongWen Li <lizw@primeton.com>
2016-08-17 16:45:59 +0800

    Update: Enable runnablejar example component project

---
+++ c-runnablejar/package.cmd
@@ -0,0 +1,5 @@
+echo "package binary"
+
+cd target
+dir
+jar cvfM0 runnablejar-1.0.0-SNAPSHOT.zip runnablejar-1.0.0-SNAPSHOT-jar-with-dependencies.jar
---
+++ c-runnablejar/package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2001-2016 Primeton Technologies, Ltd.
+# All rights reserved.
+#
+# author: ZhongWen Li (mailto:lizw@primeton.com)
+#
+
+CONTEXT_PATH=$(cd $(dirname ${0}); pwd)
+
+if [ -z "`which zip`" ]; then
+	echo "[`date`] [ERROR] zip command not found."
+	exit 1
+fi
+
+cd ${CONTEXT_PATH}/target
+zip runnablejar-1.0.0-SNAPSHOT.zip runnablejar-1.0.0-SNAPSHOT-jar-with-dependencies.jar
